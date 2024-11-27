6a5bce4b7edd98701bd693d426be44e04de3f323
Eppepa03 <162826542+Eppepa03@users.noreply.github.com>
2024-11-27 10:12:53 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const Note = require("./models/note")
 const app = express()
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method)
@@ -13,7 +15,6 @@ app.use(express.json())
 app.use(cors())
 app.use(requestLogger)
 app.use(express.static("dist"))
-
 
 let notes = [
     {
@@ -45,7 +46,9 @@ app.get("/", (request, response) => {
 })
 
 app.get("/api/notes", (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get("/api/notes/:id", (request, response) => {
@@ -92,7 +95,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
